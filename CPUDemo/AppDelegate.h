649435780b6d25e7bac086f76a8c28c91ad5d432
Alex Ao <aozhimin0811@gmail.com>
2018-02-23 17:55:17 +0800

    Add projects

---
+++ CPUDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CPUDemo
+//
+//  Created by dev-aozhimin on 2018/2/23.
+//  Copyright © 2018年 dev-aozhimin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CPUDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CPUDemo
+//
+//  Created by dev-aozhimin on 2018/2/23.
+//  Copyright © 2018年 dev-aozhimin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
